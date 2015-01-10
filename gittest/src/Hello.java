5de7c6351e017a121460e83a1cc19ef9208c049d
yemengying <391859458@qq.com>
2015-01-10 13:23:37 +0800

    乱码

--- gittest/src/Hello.java
+++ gittest/src/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.print("°®ÎÒ²»~~");
+		System.out.print("çˆ±æˆ‘ä¹ˆ~~");
 		System.out.print("love");
 		System.out.print("aas");
 
